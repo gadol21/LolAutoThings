58c1ea9faf59fe6e9804440c40dbf508031dd52f
Mexican Botten <mexicanbotten@gmail.com>
2015-07-21 17:49:09 +0300

    Get objects and tyoes

--- League/src/League/objects_handler.py
+++ League/src/League/objects_handler.py
@@ -32,6 +32,19 @@ class ObjectsHandler(object):
                     objects.append(obj)
         return objects
 
+    def names_contain(self, string):
+        """
+        :param string: the string to look for
+        :return: all the objects names contain the specific string
+        """
+        names = set()
+        for i in xrange(self.LIST_SIZE):
+            if self.engine.object_exist(i):
+                obj = LeagueObject(self.engine, i)
+                if string.lower() in obj.name.lower():
+                    names.add(obj.name)
+        return names
+
     def get_by_type(self, obj_type, obj_class=LeagueObject):
         """
         :return: list contain all the objects with the specific type
@@ -43,3 +56,16 @@ class ObjectsHandler(object):
                 if obj.type == obj_type:
                     objects.append(obj)
         return objects
+
+    def types_contain(self, string):
+        """
+        :param string: the string to look for
+        :return: all the objects types contain the specific string
+        """
+        types = set()
+        for i in xrange(self.LIST_SIZE):
+            if self.engine.object_exist(i):
+                obj = LeagueObject(self.engine, i)
+                if string.lower() in obj.type.lower():
+                    types.add(obj.type)
+        return types
