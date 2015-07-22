567dbee4e11bf845534142352c0b045fbad91a27
Mexican Botten <mexicanbotten@gmail.com>
2015-07-22 19:12:48 +0300

    New list offset

--- League/src/League/__init__.py
+++ League/src/League/__init__.py
@@ -1,4 +1,4 @@
 from champion import Champion
 from objects_handler import ObjectsHandler
 from league_object import LeagueObject
-from functions import write_to_chat
+#from functions import write_to_chat
