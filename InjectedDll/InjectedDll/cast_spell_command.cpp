279d01fd15981ded13390fa1f9883fa6110bcb81
gadol21 <gadol21dev@gmail.com>
2015-07-23 01:00:55 +0300

    updates on cast_spell_command

--- InjectedDll/InjectedDll/cast_spell_command.cpp
+++ InjectedDll/InjectedDll/cast_spell_command.cpp
@@ -1,9 +1,24 @@
 #include "cast_spell_command.h"
+#include "lol_helper.h"
+#include "offsets.h"
+#include <stdexcept>
 
 CastSpellCommand::CastSpellCommand(const char* buffer, size_t buffer_len) {
-
+	if (buffer_len != sizeof(cast_spell_message)) {
+		throw std::invalid_argument("CastSpell: wrong buffer length");
+	}
+	const cast_spell_message* message = reinterpret_cast<const cast_spell_message*>(buffer);
+	m_main_champion = message->main_champion;
+	m_spell_index = message->spell_index;
+	m_source_pos = message->source_pos;
+	m_target_pos = message->target_pos;
+	m_target_unit = message->target_unit;
 }
 
 void CastSpellCommand::operator()() {
+	cast_spell_func CastSpell = reinterpret_cast<cast_spell_func>(LolHelper::get_lol_base() + offsets::cast_spell);
+	spellmanager* manager = reinterpret_cast<spellmanager*>(m_main_champion + offsets::cast_spell_spellmanager);
 
+	//__asm mov ecx, UNKNOWN;
+	CastSpell(manager->spells[m_spell_index], m_spell_index, m_target_pos, m_source_pos, m_target_unit);
 }
--- InjectedDll/InjectedDll/cast_spell_command.h
+++ InjectedDll/InjectedDll/cast_spell_command.h
@@ -15,11 +15,19 @@ typedef struct {
  * (0=Q;1=W;2=E;3=R;4=D;5=F)
  */
 typedef struct {
-	void* spell[6];
+	void* spells[6];
 } spellmanager;
 
 /// this function is __thiscall. make sure to pass ecx explicitly before calling it
 typedef void(__stdcall * cast_spell_func)(void* spell_information, uint32_t spell_index, position& target, position& source, uint32_t target_unit);
+
+typedef struct {
+	uint32_t main_champion;
+	uint8_t spell_index;
+	position target_pos;
+	position source_pos;
+	uint32_t target_unit;
+} cast_spell_message;
 
 /**
  * Sends a CastSpell command - cast a given spell to a given target. protocol:
@@ -27,18 +35,25 @@ typedef void(__stdcall * cast_spell_func)(void* spell_information, uint32_t spel
  * uint32_t main_champion (our hero)
  * uint8_t spell_index (0=Q;1=W;2=E;3=R;4=D;5=F)
  * position target_pos
- * position our_pos
+ * position source_pos (for most spells it is our pos)
  * uint32_t target_unit
  */
 class CastSpellCommand : public Command {
 public:
+	/**
+	 * Parses a buffer with a given length.
+	 * @throws invalid_argument if buffer is malformatted
+	 */
 	CastSpellCommand(const char* buffer, size_t buffer_len);
 
 	virtual void operator()();
 
 private:
 	uint32_t m_main_champion;
-	uint8_t spell_index;
+	uint8_t m_spell_index;
+	position m_target_pos;
+	position m_source_pos;
+	uint32_t m_target_unit;
 };
 
 #endif // ndef __CAST_SPELL_COMMAND_H
