04c26b5ad3f684fdde186f0271f316692823030e
gadol21 <gadol21dev@gmail.com>
2015-07-22 23:31:08 +0300

    started implementing cast_spell_command

--- InjectedDll/InjectedDll/cast_spell_command.h
+++ InjectedDll/InjectedDll/cast_spell_command.h
@@ -2,12 +2,24 @@
 #define __CAST_SPELL_COMMAND_H
 
 #include "command.h"
+#include <cstdint>
 
 typedef struct {
 	float x;
 	float z;
 	float y;
 } position;
+
+/**
+ * spellmanager consists of 6 pointers to the 6 spells
+ * (0=Q;1=W;2=E;3=R;4=D;5=F)
+ */
+typedef struct {
+	void* spell[6];
+} spellmanager;
+
+/// this function is __thiscall. make sure to pass ecx explicitly before calling it
+typedef void(__stdcall * cast_spell_func)(void* spell_information, uint32_t spell_index, position& target, position& source, uint32_t target_unit);
 
 /**
  * Sends a CastSpell command - cast a given spell to a given target. protocol:
@@ -23,6 +35,10 @@ public:
 	CastSpellCommand(const char* buffer, size_t buffer_len);
 
 	virtual void operator()();
+
+private:
+	uint32_t m_main_champion;
+	uint8_t spell_index;
 };
 
 #endif // ndef __CAST_SPELL_COMMAND_H
--- InjectedDll/InjectedDll/offsets.h
+++ InjectedDll/InjectedDll/offsets.h
@@ -14,7 +14,9 @@ const uint32_t send_chat_message_this_message_offset = 0xe8;// updated to 5.14
 const uint32_t floating_text = 0x99E580;					// updated to 5.14
 const uint32_t floating_text_magic = 0x111CCF0;				// updated to 5.14
 
-const uint32_t cast_spell = 0xbf61C0;			// updated to 5.14
+const uint32_t cast_spell = 0xbf61C0;						// updated to 5.14
+// this offset is from main_unit base address
+const uint32_t cast_spell_spellmanager = 0x2730;
 
 } // namespace offsets
 
