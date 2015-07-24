f37b19978b494e81511519ce4b505f416398bafc
gadol21 <gadol21dev@gmail.com>
2015-07-24 17:49:28 +0300

    all offsets found

--- InjectedDll/InjectedDll/offsets.h
+++ InjectedDll/InjectedDll/offsets.h
@@ -11,16 +11,16 @@ const uint32_t send_chat_message = 0x777fd0;				// 5.14 2
 const uint32_t send_chat_message_this = 0x2d9c538;			// 5.14 2
 const uint32_t send_chat_message_this_message_offset = 0xe8;// 5.14 2
 
-const uint32_t floating_text = 0x1099613;					// 5.14_2
-const uint32_t floating_text_magic = 0x1BCF2F3;				// 5.14_2
+const uint32_t floating_text = 0x5DF060;					// 5.14 2
+const uint32_t floating_text_magic = 0x1114D40;				// 5.14 2
 
 const uint32_t cast_spell = 0xbf6160;						// 5.14 2
 // these offsets are from main_unit base address
 const uint32_t cast_spell_this = 0x22c8;					// 5.14 2
-const uint32_t cast_spell_spellmanager = 0x27e0;			// 5.14
-const uint32_t cast_spell_target_unit_offset = 0xf8;		// 5.14
+const uint32_t cast_spell_spellmanager = 0x27e0;			// 5.14 2
+const uint32_t cast_spell_target_unit_offset = 0xf8;		// 5.14 2
 
-const uint32_t attackmove = 0x5f65d0;						// 5.14
+const uint32_t attackmove = 0xa29983;						// 5.14 2
 
 } // namespace offsets
 
