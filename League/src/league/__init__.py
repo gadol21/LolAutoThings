1ac574881adcdd379f540a43d7ce50e6a8e99a9f
gadol21 <gadol21dev@gmail.com>
2015-07-25 12:07:28 +0300

    changes

--- League/src/league/__init__.py
+++ League/src/league/__init__.py
@@ -3,4 +3,4 @@ from minion import Minion
 from objects_handler import (get_by_name, get_by_type, names_contain,
                              types_contain, engine, get, get_me)
 from league_object import LeagueObject
-from functions import write_to_chat, cast_spell
+from functions import write_to_chat
--- League/src/league/champion.py
+++ League/src/league/champion.py
@@ -1,6 +1,7 @@
 from field_types import Int, Short, Byte, Float, NullTerminatedString, LengthedString
 from league_object import LeagueObject
 from spell_manager import SpellManager
+import functions
 
 
 class Champion(LeagueObject):
@@ -19,3 +20,29 @@ class Champion(LeagueObject):
         """
         spell_manager_offset = 0x27e0
         return SpellManager(self._engine, self.addr + spell_manager_offset)
+
+    def cast_skillshot(self, skill_id, target_pos, src_pos=None):
+        """
+        like ezreal q
+        :param skill_id: the skill id - 0-q, 1-w, 2-e, 3-r, 4-d, 5-f
+        :param target_pos: tuple (x, z, y)
+        :param src_pos: for skills like rumble ult or victor e the requires a start pos
+        """
+        if src_pos is None:
+            src_pos = self.position
+        functions.cast_spell(self.addr, skill_id, target_pos, src_pos, 0)
+
+    def cast_self_spell(self, skill_id):
+        """
+        spells like hecarim q
+        :param skill_id: the skill id - 0-q, 1-w, 2-e, 3-r, 4-d, 5-f
+        """
+        functions.cast_spell(self.addr, skill_id, 0, (self.x, self.z, self.y), 0)
+
+    def cast_target(self, skill_id, target):
+        """
+        spells like veigar's ult
+        :param skill_id: the skill id - 0-q, 1-w, 2-e, 3-r, 4-d, 5-f
+        target - LeagueObject of the target
+        """
+        functions.cast_spell(self.addr, skill_id, (target.x, target.z, target.y), (self.x, self.z, self.y), target.addr)
