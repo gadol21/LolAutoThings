d52279ce4d91c0470f4b5a1019eeaaaa965560c1
Mexican Botten <mexicanbotten@gmail.com>
2015-07-25 19:50:50 +0300

    fixed setup.py

--- League/src/setup.py
+++ League/src/setup.py
@@ -4,5 +4,6 @@ setup(
     name='league',
     version='1',
     description='league of Legends swag',
-    packages=find_packages()
+    packages=find_packages(),
+	data_files = ['league/_objreader.pyd', 'league/InjectedDll.dll']
 )
---
+++ README.txt
@@ -0,0 +1,10 @@
+install:
+cd directory
+cd league/src
+python setup.py install
+run:
+cd directory
+run cmd
+python runner.py
+
+have fun
