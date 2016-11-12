902f97dd4492ecef7ac7d726b2c8b5bda555a214
gadol21 <gadol21dev@gmail.com>
2016-11-12 11:51:08 +0200

    Should also work for enemy wards now

--- warddetector.py
+++ warddetector.py
@@ -11,18 +11,19 @@ class WardDetector(object):
 
     def init(self):
         self.last = time.time()
-        self.wards = []
+        self.wards = [] # tuple of ward, ward time first time seen, and first time seen
         for ward in get_by_name('SightWard'):
-            self.wards.append(ward)
+            self.wards.append((ward, ward.ward_time, time.time()))
         for ward in get_by_name('VisionWard'):
-            self.wards.append(ward)
+            self.wards.append((ward, ward.ward_time, time.time()))
 
     def step(self):
         if self.active:
             current_time = time.time()
             if current_time - self.last > 1:
-                for ward in self.wards:
-                    ward.floating_text(26, 'Ward ' + str(int(ward.ward_time)))
+                for ward, ward_initial_time, ward_first_seen in self.wards:
+                    time_passed = time.time() - ward_first_seen
+                    ward.floating_text(26, 'Ward ' + str(int(ward_initial_time - time_passed)))
                 self.last = time.time()
 
     def on_object_add(self, obj_addr):
@@ -30,12 +31,12 @@ class WardDetector(object):
         # check if obj is ward
         obj_name = obj.name
         if obj_name == 'SightWard' or obj_name == 'VisionWard':
-            self.wards.append(obj)
+            self.wards.append((obj, obj.ward_time, time.time()))
         
     def on_object_remove(self, obj_addr):
         # check if anyward in the list got this addr.
         for i in xrange(len(self.wards)):
-            if self.wards[i].addr == obj_addr:
+            if self.wards[i][0].addr == obj_addr:
                 self.wards.remove(self.wards[i])
                 return
 
